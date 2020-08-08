5a8f4352f03799ec89271556e3963085cb7be685
SonghoYun <itlockit@gmail.com>
2020-08-08 17:43:06 +0900

    db connect & print sigin error message

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
-
-console.log("process.env.MONGO_URL : ", process.env.MONGO_URL);
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  `${process.env.DB_HOST}:${process.env.DB_PASSWOD}@${process.env.MONGO_URL}`,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 const db = mongoose.connection;
 const handleOpen = () => console.log("Connected to DB");
